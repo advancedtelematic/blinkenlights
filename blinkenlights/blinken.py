237db51eaa0924a67d8530729270a639444c54aa
Ben Luck <benjamin.luck@here.com>
2019-09-19 14:54:46 +0200

    SOS

--- blinkenlights/blinken.py
+++ blinkenlights/blinken.py
@@ -8,10 +8,41 @@ ledPin = 12
 
 GPIO.setup(ledPin, GPIO.OUT)
 
-for i in range(5):
+def dit():
 	print("LED turning on.")
 	GPIO.output(ledPin, GPIO.HIGH)
 	time.sleep(0.5)
 	print("LED turning off.")
 	GPIO.output(ledPin, GPIO.LOW) 
 	time.sleep(0.5)
+
+def dat():
+	print("LED turning on.")
+	GPIO.output(ledPin, GPIO.HIGH)
+	time.sleep(1.5)
+	print("LED turning off.")
+	GPIO.output(ledPin, GPIO.LOW) 
+	time.sleep(0.5)
+
+def char_space():
+	time.sleep(1)
+
+def word_space():
+	time.sleep(7)
+
+def sos():
+	dit()
+	dit()
+	dit()
+	char_space()
+	dat()
+	dat()
+	dat()
+	char_space()
+	dit()
+	dit()
+	dit()
+	word_space()
+
+while True:
+	sos()
