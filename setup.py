dde1557972c045666adc721bbf607d51c43bf64c
Alex Humphreys <alex.humphreys@here.com>
2019-09-19 13:55:02 +0200

    Add setup.py
    Signed-off-by: Alex Humphreys <alex.humphreys@here.com>

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='blinkenlights',
+   version='0.0.1',
+   description='Some lights doing stuff',
+   author='OTA',
+   author_email='OTA@example.com',
+   packages=['blinkenlights'],  #same as name
+   # install_requires=['bar', 'greek'], #external packages as dependencies
+)
